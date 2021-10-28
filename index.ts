8037b0016a48353ccb547f251f9becce2c033d10
ArturV19 <ArturVoekov@mail.ru>
2021-10-28 09:09:17 +0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+import KEY from "./key.js";
+
+import {Request, Response} from "express";
+import fetch from "node-fetch";
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+app.set('view engine', 'ejs');
+
+
+const urlEncodedParser = bodyParser.urlencoded({
+    extended: false,
+});
+
+
+app.get('/', urlEncodedParser, (req: Request, res: Response) => {
+    res.render('pages/index');
+});
+
+type JsonType = {
+    cod: number;
+    message: string;
+    main: {
+        temp: number;
+        pressure: number;
+    }
+}
+
+app.get('/weather', urlEncodedParser, (req: Request, res: Response) => {
+    console.log(req.query);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${KEY}&units=metric`)
+        .then(res => res.json())
+        .then(
+            (json) => {
+                const data = json as JsonType;
+                if (data.cod == 200) {
+                    res.render('pages/show_weather_info', {weather_data: data.main, city: req.query.city});
+                } else {
+                    res.render('pages/error', {error_code: data.cod, error_message: data.message });
+                }
+            }
+        );
+
+});
+
+app.listen(PORT);
+console.log(`Server run on port ${PORT}`);
---
+++ key.js
@@ -0,0 +1,3 @@
+export let KEY = 'c15bf3b818db24570a7b7fc8189ff791';
+
+export default KEY;
