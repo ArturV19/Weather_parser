5690b84468b84a16ad466d96a4995b82ac9b690f
ArturV19 <arturvoekov@mail.ru>
2021-11-01 11:02:30 +0400

    Реализация рендера страницы и последующего запроса

--- index.ts
+++ index.ts
@@ -20,30 +20,25 @@ app.get('/', urlEncodedParser, (req: Request, res: Response) => {
     res.render('pages/index');
 });
 
-type JsonType = {
-    cod: number;
-    message: string;
-    main: {
-        temp: number;
-        pressure: number;
-    }
-}
+// type JsonType = {
+//     cod: number;
+//     message: string;
+//     main: {
+//         temp: number;
+//         pressure: number;
+//     }
+// }
 
-app.get('/weather', urlEncodedParser, (req: Request, res: Response) => {
-    console.log(req.query);
+app.get('/weather_data', (req, res) => {
+    console.log(res.req);
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${KEY}&units=metric`)
         .then(res => res.json())
-        .then(
-            (json) => {
-                const data = json as JsonType;
-                if (data.cod == 200) {
-                    res.render('pages/show_weather_info', {weather_data: data.main, city: req.query.city});
-                } else {
-                    res.render('pages/error', {error_code: data.cod, error_message: data.message });
-                }
-            }
-        );
+        .then((data) => res.send(data));
+    //  https://medium.com/@iampika/es6-arrow-functions-syntax-and-lexical-scoping-d061732071e7
+});
 
+app.get('/weather', urlEncodedParser, (req: Request, res: Response) => {
+    res.render('pages/show_weather_info_proxy', {city: req.query.city});
 });
 
 app.listen(PORT);
